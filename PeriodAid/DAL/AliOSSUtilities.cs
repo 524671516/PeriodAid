cde062b25f0ffd1b7cb44f6cd1e89e5fe0f86d8b
许亦安 <cpcw_xuyian@hotmail.com>
2016-03-29 11:23:37 +0800

    0329temp

---
+++ PeriodAid/DAL/AliOSSUtilities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Aliyun.OSS;
+using System.IO;
+namespace PeriodAid.DAL
+{
+    public class AliOSSUtilities
+    {
+        private const string AccessKey = "04nGoi0uof51Tbcn";
+        private const string AccessSecret = "1z4XQF95OBX5nnMfcDukoItI85tB6s";
+        private const string endpoint = "http://offlinesales.oss-cn-hangzhou.aliyuncs.com";
+        private OssClient client;
+        public AliOSSUtilities()
+        {
+            client = new OssClient(endpoint, AccessKey, AccessSecret);
+        }
+        public void PutObject(byte[] content, string filename)
+        {
+            MemoryStream ms = new MemoryStream(content);
+            client.PutObject("offlinesales", filename, ms);
+        }
+    }
+}
