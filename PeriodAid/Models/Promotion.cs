99764d883e8980ee90901a5c2e509a85801f0e86
许亦安 <cpcw_xuyian@hotmail.com>
2016-05-23 17:52:00 +0800

    0523：淘宝返现活动(UNI_Event)

--- PeriodAid/Models/Promotion.cs
+++ PeriodAid/Models/Promotion.cs
@@ -3,6 +3,7 @@ using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Collections.Generic;
 namespace PeriodAid.Models
 {
     public partial class Promotion : DbContext
@@ -14,8 +15,16 @@ namespace PeriodAid.Models
 
         public virtual DbSet<Promotion_TJH> Promotion_TJH { get; set; }
         public virtual DbSet<P_Presents> P_Presents { get; set; }
+        public virtual DbSet<UNI_Group> UNI_Group { get; set; }
+        public virtual DbSet<UNI_MchBill> UNI_MchBill { get; set; }
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UNI_Group>()
+                .HasMany(e => e.UNI_MchBill)
+                .WithRequired(e => e.UNI_Group)
+                .HasForeignKey(e => e.GroupId);
         }
         
     }
@@ -103,5 +112,72 @@ namespace PeriodAid.Models
 
         [StringLength(128)]
         public string plattform_code { get; set; }
+    }
+
+    public partial class UNI_Group
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string GroupCode { get; set; }
+
+        [Required]
+        [StringLength(32)]
+        public string GroupName { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<UNI_MchBill> UNI_MchBill { get; set; }
+    }
+
+    public partial class UNI_MchBill
+    {
+        public int Id { get; set; }
+
+        public int GroupId { get; set; }
+
+        public virtual UNI_Group UNI_Group { get; set; }
+
+        [StringLength(128)]
+        public string NickName { get; set; }
+
+        [StringLength(32)]
+        [Required]
+        public string Mobile { get; set; }
+
+        [StringLength(128)]
+        public string UploadImg { get; set; }
+
+        [StringLength(32)]
+        public string Province { get; set; }
+
+        [StringLength(32)]
+        public string City { get; set; }
+
+        public bool Sex { get; set; }
+
+        [StringLength(512)]
+        public string ImgUrl { get; set; }
+
+        [StringLength(32)]
+        [Required]
+        public string OpenId { get; set; }
+
+        public int Status { get; set; }
+
+        [StringLength(32)]
+        public string StatusCode { get; set; }
+
+        public int Mch_Amount { get; set; }
+
+        [StringLength(64)]
+        public string platform_code { get; set; }
+
+        [StringLength(128)]
+        public string MchBillNo { get; set; }
+
+        public DateTime SendTime { get; set; }
+
+        public DateTime? ReceiveTime { get; set; }
     }
 }
