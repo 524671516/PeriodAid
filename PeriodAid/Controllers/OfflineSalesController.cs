76feefeeb1883925e691791f03dca6d5654d2d65
许亦安 <cpcw_xuyian@hotmail.com>
2016-06-03 14:29:06 +0800

    rr

--- PeriodAid/Controllers/OfflineSalesController.cs
+++ PeriodAid/Controllers/OfflineSalesController.cs
@@ -2895,7 +2895,7 @@ namespace PeriodAid.Controllers
                 }
                 else if (type == "day")
                 {
-                    DateTime et = Convert.ToDateTime(enddate);
+                    
                     string sql = "SELECT T1.Date, T2.StoreSystem, SUM(T1.Salary) as Salary, SUM(T1.Debit) as Debit, SUM(T1.Bonus) as Bonus FROM [Off_SalesInfo_Daily] as T1 left join [Off_Store] as T2 on T1.StoreId = T2.Id " +
                         "where Date>= '" + st.ToString("yyyy-MM-dd") + "' and Date< '" + et.ToString("yyyy-MM-dd") + "' and T2.StoreSystem like '" + storesystem + "' and T2.Off_System_Id = " + user.DefaultSystemId + " " +
                         "group by T2.StoreSystem, T1.Date";
@@ -2954,7 +2954,7 @@ namespace PeriodAid.Controllers
                 if (type == "month")
                 {
                     DateTime et = st.AddMonths(1);
-                    string sql = "SELECT CONVERT(char(7), T1.Date, 120) as Date, T2.StoreSystem, SUM(T1.Salary) as Salary, SUM(T1.Debit) as Debit, SUM(T1.Bonus) as Bonus FROM [Off_SalesInfo_Daily] as T1 left join [Off_Store] as T2 on T1.StoreId = T2.Id " +
+                    string sql = "SELECT  CONVERT(datetime, CONVERT(char(7), T1.Date, 120)+'-01') as Date, T2.StoreSystem, SUM(T1.Salary) as Salary, SUM(T1.Debit) as Debit, SUM(T1.Bonus) as Bonus FROM [Off_SalesInfo_Daily] as T1 left join [Off_Store] as T2 on T1.StoreId = T2.Id " +
                         "where Date>= '" + st.ToString("yyyy-MM-01") + "' and Date< '" + et.ToString("yyyy-MM-01") + "' and T1.StoreId " + storeid + " and T2.Off_System_Id = " + user.DefaultSystemId + " " +
                         "group by T2.StoreSystem, CONVERT(char(7), T1.Date, 120)";
                     var data = offlineDB.Database.SqlQuery<StoreSystem_Salary_Statistic>(sql);
