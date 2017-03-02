b5f00c1299176e148145e8d0263ff76e3691591f
尚涛 王 <wangst22105@outlook.com>
2017-03-02 12:11:27 +0800

    changes

---
+++ PeriodAid/Controllers/WebContentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeriodAid.Controllers
+{
+    public class WebContentController : Controller
+    {
+        /*活动列表*/
+        // GET: WebContent
+        public ActionResult WebContentIndex()
+        {
+            return View();
+        }
+        //GET:WebEventList_Ajax
+        public ActionResult WebEventsList_Ajax()
+        {
+            return PartialView();
+        }
+        //GET:CreateWebEventPartial
+        public ActionResult CreateWebEventPartial()
+        {
+            return PartialView();
+        }
+        //[HttpPost,ValidateAntiForgeryToken]
+        //public ActionResult CreateWebEventPartial()
+        //{
+
+        //}
+    }
+}
