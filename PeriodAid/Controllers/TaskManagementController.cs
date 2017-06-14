28e8c62cacc440a2e3a7f5cabc3c144a06f64381
尚涛 王 <wangst22105@outlook.com>
2017-06-14 15:09:53 +0800

    changes

---
+++ PeriodAid/Controllers/TaskManagementController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeriodAid.Controllers
+{
+    public class TaskManagementController : Controller
+    {
+        // GET: TaskManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //获取星标任务列表
+        public PartialViewResult Personal_StarTaskListPartial()
+        {
+            return PartialView();
+        }
+        //获取行动任务列表
+        public PartialViewResult Personal_ActiveTaskListPartial()
+        {
+            return PartialView();
+        }
+        //获取已完成任务列表
+        public PartialViewResult Personal_FinishTaskListPartial()
+        {
+            return PartialView();
+        }
+
+        //项目详情
+        public ActionResult Task_Detail(string TaskId)
+        {
+            return View();
+        }
+    }
+}
---
+++ PeriodAid/Scripts/TaskManagement.js
@@ -0,0 +1,41 @@
+﻿$(function () {
+    GetStarTask("#personal_star_task")
+    GetActiveTask("#personal_active_task")
+    GetFinishTask("#personal_finish_task")
+    console.log(1);
+    $("#personal_active_task").on("click", ".card-item", function () {
+        var linkid = $(this).attr("data-link")
+        if (linkid) {
+            location.href = "/TaskManagement/Task_Detail?TaskId=" + linkid
+        }
+    })
+    $("#personal_active_task").on("click", ".card-header-right", function () {
+        alert(1);
+        return false;
+    })
+
+});
+function GetStarTask(container) {
+    $.ajax({
+        url: "/TaskManagement/Personal_StarTaskListPartial",
+        success: function (data) {
+            $(container).html(data);
+        }
+    })
+}
+function GetActiveTask(container) {
+    $.ajax({
+        url: "/TaskManagement/Personal_ActiveTaskListPartial",
+        success: function (data) {
+            $(container).html(data);
+        }
+    })
+}
+function GetFinishTask(container) {
+    $.ajax({
+        url: "/TaskManagement/Personal_FinishTaskListPartial",
+        success: function (data) {
+            $(container).html(data);
+        }
+    })
+}
