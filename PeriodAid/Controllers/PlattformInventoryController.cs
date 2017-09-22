46f72f68817a352a1b923f281e47a5f477a653bc
xuyian <xuyian@XUYIAN-PC>
2017-09-22 12:11:02 +0800

    changes

---
+++ PeriodAid/Controllers/PlattformInventoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeriodAid.Controllers
+{
+    public class PlattformInventoryController : Controller
+    {
+        // GET: PlattformInventory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
