86ea8d2bde70311d48d1a5452f526eb0286d2ec9
myron <1207278908@qq.com>
2017-09-25 10:07:27 +0800

    5

--- PeriodAid/Controllers/PlattformInventoryController.cs
+++ PeriodAid/Controllers/PlattformInventoryController.cs
@@ -1,12 +1,14 @@
 ﻿using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
-using PeriodAid.UnityTool;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.OleDb;
 
 namespace PeriodAid.Controllers
 {
@@ -22,38 +24,92 @@ namespace PeriodAid.Controllers
             return View();
         }
 
+        // 将Excel导入数据库  
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        [HandleError(View = "~/Views/Shared/Error.cshtml")]
-        public ActionResult Browse(HttpPostedFileBase file)
+        public ActionResult LeadingIn(HttpPostedFileBase filebase)
         {
-
-            if (string.Empty.Equals(file.FileName) || ".xlsx" != Path.GetExtension(file.FileName))
+            HttpPostedFileBase file = Request.Files["FileUpload"];//获取上传的文件  
+            string FileName;
+            string savePath;
+            if (file == null || file.ContentLength <= 0)
             {
-                throw new ArgumentException("当前文件格式不正确,请确保正确的Excel文件格式!");
+                ViewBag.error = "文件不能为空";
+                return View();
+            }
+            else
+            {
+                string filename = Path.GetFileName(file.FileName);
+                int filesize = file.ContentLength;//获取上传文件的大小单位为字节byte  
+                string fileEx = System.IO.Path.GetExtension(filename);//获取上传文件的扩展名  
+                string NoFileName = System.IO.Path.GetFileNameWithoutExtension(filename);//获取无扩展名的文件名  
+                int Maxsize = 10000 * 1024;//定义上传文件的最大空间大小为10M  
+                string FileType = ".xls,.xlsx";//定义上传文件的类型字符串  
+
+                FileName = NoFileName + DateTime.Now.ToString("yyyyMMddhhmmss") + fileEx;
+                if (!FileType.Contains(fileEx))
+                {
+                    ViewBag.error = "文件类型不对，只能导入xls和xlsx格式的文件";
+                    return View();
+                }
+                if (filesize >= Maxsize)
+                {
+                    ViewBag.error = "上传文件超过10M，不能上传";
+                    return View();
+                }
+                string path = AppDomain.CurrentDomain.BaseDirectory;
+                savePath = Path.Combine(path, FileName);
+                file.SaveAs(savePath);
             }
 
-            var severPath = this.Server.MapPath("/files/"); //获取当前虚拟文件路径
+            string result = string.Empty;
+            string strConn;
+            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + savePath + "; " + "Extended Properties=Excel 8.0;";  
 
-            var savePath = Path.Combine(severPath, file.FileName); //拼接保存文件路径
+            //strConn = "Provider=Microsoft.Ace.OleDb.12.0;" + "data source=" + savePath + ";Extended Properties='Excel 12.0; HDR=Yes; IMEX=1'"; //此连接可以操作.xls与.xlsx文件 (支持Excel2003 和 Excel2007 的连接字符串)  
+                                                                                                                                               //备注： "HDR=yes;"是说Excel文件的第一行是列名而不是数据，"HDR=No;"正好与前面的相反。  
+                                                                                                                                               //      "IMEX=1 "如果列中的数据类型不一致，使用"IMEX=1"可必免数据类型冲突。  
 
+            //OleDbDataAdapter myCommand = new OleDbDataAdapter("select * from [Sheet1$]", strConn);  
+            DataSet myDataSet = new DataSet();
             try
             {
-                file.SaveAs(savePath);
-                stus = ExcelHelper.ReadExcelToEntityList<Student>(savePath);
-                ViewBag.Data = stus;
-                return View("Index");
+                //连接串  
+                //string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "DataSource=" + Path + ";" + "ExtendedProperties=Excel8.0;";  
+                OleDbConnection conn = new OleDbConnection(strConn);
+                conn.Open();
+                //返回Excel的架构，包括各个sheet表的名称,类型，创建时间和修改时间等　  
+                DataTable dtSheetName = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "Table" });
+                //包含excel中表名的字符串数组  
+                string[] strTableNames = new string[dtSheetName.Rows.Count];
+                for (int k = 0; k < dtSheetName.Rows.Count; k++)
+                {
+                    strTableNames[k] = dtSheetName.Rows[k]["TABLE_NAME"].ToString();
+                }
+                OleDbDataAdapter myCommand = null;
+                DataTable dt = new DataTable();
+                //从指定的表明查询数据,可先把所有表明列出来供用户选择  
+                string strExcel = "select*from[" + strTableNames[0] + "]";
+                myCommand = new OleDbDataAdapter(strExcel, strConn);
+
+
+
+
+                //myCommand.Fill(dt);  
+                myCommand.Fill(myDataSet, "ExcelInfo");
             }
-            finally
+            catch (Exception ex)
             {
-                System.IO.File.Delete(savePath);//每次上传完毕删除文件
+                ViewBag.error = ex.Message;
+                return View();
             }
+            DataTable table = myDataSet.Tables["ExcelInfo"].DefaultView.ToTable();
 
-        }
 
-        [HandleError(View = "~/Views/Shared/Error.cshtml")]
-        public ActionResult Upload() {
-            return View("UploadSuccess");
+
+            ViewBag.error = "上传成功";
+            System.Threading.Thread.Sleep(2000);
+            return View();
+            //return RedirectToAction("DataImport");  
         }
 
 
