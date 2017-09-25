a7afed8f6c69e219e6a5d56cc7280b926b10e077
myron <1207278908@qq.com>
2017-09-25 10:44:45 +0800

    8

--- PeriodAid/Controllers/PlattformInventoryController.cs
+++ PeriodAid/Controllers/PlattformInventoryController.cs
@@ -1,6 +1,5 @@
 ﻿using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
-using PeriodAid.UnityTool;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,7 +7,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CsvHelper;
-using System.IO;
 using System.Text;
 using Microsoft.AspNet.Identity.Owin;
 using PeriodAid.Models;
@@ -110,38 +108,11 @@ namespace PeriodAid.Controllers
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [HandleError(View = "~/Views/Shared/Error.cshtml")]
-        public ActionResult Browse(HttpPostedFileBase file)
-        {
-
-            if (string.Empty.Equals(file.FileName) || ".xlsx" != Path.GetExtension(file.FileName))
-            {
-                throw new ArgumentException("当前文件格式不正确,请确保正确的Excel文件格式!");
-            }
-
-            var severPath = this.Server.MapPath("/files/"); //获取当前虚拟文件路径
-
-            var savePath = Path.Combine(severPath, file.FileName); //拼接保存文件路径
-
-            try
-            {
-                file.SaveAs(savePath);
-                stus = ExcelHelper.ReadExcelToEntityList<Student>(savePath);
-                ViewBag.Data = stus;
-                return View("Index");
-            }
-            finally
-            {
-                System.IO.File.Delete(savePath);//每次上传完毕删除文件
-            }
-
-        }
-
-        [HandleError(View = "~/Views/Shared/Error.cshtml")]
-        public ActionResult Upload() {
-            return View("UploadSuccess");
+        public ActionResult PlattformInventory_form() {
+            var storage = from m in _db.SS_Storage
+                          select m;
+            ViewBag.storage = storage;
+            return View();
         }
 
 
