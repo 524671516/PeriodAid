f446884911cf7dde3ff4649b57398ca6377a5fb8
524671516 <524671516@qq.com>
2017-09-25 10:12:09 +0800

    9251

--- PeriodAid/Controllers/PlattformInventoryController.cs
+++ PeriodAid/Controllers/PlattformInventoryController.cs
@@ -14,48 +14,9 @@ namespace PeriodAid.Controllers
     {
         // GET: PlattformInventory
         public ActionResult Index()
-        {           
-            return View();
-        }
-
-        public ActionResult LeadingIn() {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        [HandleError(View = "~/Views/Shared/Error.cshtml")]
-        public ActionResult Browse(HttpPostedFileBase file)
         {
-
-            if (string.Empty.Equals(file.FileName) || ".xlsx" != Path.GetExtension(file.FileName))
-            {
-                throw new ArgumentException("当前文件格式不正确,请确保正确的Excel文件格式!");
-            }
-
-            var severPath = this.Server.MapPath("/files/"); //获取当前虚拟文件路径
-
-            var savePath = Path.Combine(severPath, file.FileName); //拼接保存文件路径
-
-            try
-            {
-                file.SaveAs(savePath);
-                stus = ExcelHelper.ReadExcelToEntityList<Student>(savePath);
-                ViewBag.Data = stus;
-                return View("Index");
-            }
-            finally
-            {
-                System.IO.File.Delete(savePath);//每次上传完毕删除文件
-            }
-
+            return View();
         }
-
-        [HandleError(View = "~/Views/Shared/Error.cshtml")]
-        public ActionResult Upload() {
-            return View("UploadSuccess");
-        }
-
 
     }
-}
+}  
