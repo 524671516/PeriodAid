0c62dff601770bd31e56c37e067ca101cb7c1599
myron <1207278908@qq.com>
2017-12-06 09:39:18 +0800

    1

---
+++ PeriodAid/Controllers/SalesProcessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PeriodAid.Controllers
+{
+    public class SalesProcessController : Controller
+    {
+        // GET: SalesProcess
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
