1b8122ff55dc33e7cba993a7058909bb1ed760c6
myron <1207278908@qq.com>
2017-12-08 15:33:42 +0800

    1

--- PeriodAid/Controllers/SalesProcessController.cs
+++ PeriodAid/Controllers/SalesProcessController.cs
@@ -425,54 +425,54 @@ namespace PeriodAid.Controllers
 
         }
 
-        //public ActionResult QuotedList(int clientId)
-        //{
-        //    var quoted = from m in _db.SP_Quoted
-        //                 where m.Client_Id == clientId
-        //                 select m;
-        //    ViewBag.Quoted = quoted;
-        //    return View();
-        //}
+        public ActionResult QuotedList(int clientId)
+        {
+            var quoted = from m in _db.SP_SalesSystem
+                         where m.Client_Id == clientId
+                         select m;
+            ViewBag.Quoted = quoted;
+            return View();
+        }
 
-        //public ActionResult QuotedListPartial(string query,int clientId)
-        //{
-        //    if (query != null)
-        //    {
-        //        if (query != "")
-        //        {
-        //            var product = (from m in _db.SP_Quoted
-        //                           where m.Client_Id == clientId
-        //                           select m);
-        //            var SearchResult = from m in product
-        //                               where m.SP_Product.Item_Name.Contains(query) || m.SP_Product.Item_Code.Contains(query) || m.SP_Product.System_Code.Contains(query)
-        //                               orderby m.Id descending
-        //                               select m;
-        //            return PartialView(SearchResult);
-        //        }
-        //        else
-        //        {
-        //            var SearchResult = from m in _db.SP_Quoted
-        //                               where m.Client_Id == clientId
-        //                               orderby m.Id descending
-        //                               select m;
-        //            return PartialView(SearchResult);
-        //        }
+        public ActionResult QuotedListPartial(string query, int clientId)
+        {
+            if (query != null)
+            {
+                if (query != "")
+                {
+                    var product = (from m in _db.SP_SalesSystem
+                                   where m.Client_Id == clientId
+                                   select m);
+                    var SearchResult = from m in product
+                                       where m.SP_Product.Item_Name.Contains(query) || m.SP_Product.Item_Code.Contains(query) || m.SP_Product.System_Code.Contains(query)
+                                       orderby m.Id descending
+                                       select m;
+                    return PartialView(SearchResult);
+                }
+                else
+                {
+                    var SearchResult = from m in _db.SP_SalesSystem
+                                       where m.Client_Id == clientId
+                                       orderby m.Id descending
+                                       select m;
+                    return PartialView(SearchResult);
+                }
 
-        //    }
-        //    else
-        //    {
-        //        var productlist = from m in _db.SP_Quoted
-        //                          where m.Client_Id == clientId
-        //                          orderby m.Id descending
-        //                          select m;
-        //        return PartialView(productlist);
-        //    }
-        //}
+            }
+            else
+            {
+                var productlist = from m in _db.SP_SalesSystem
+                                  where m.Client_Id == clientId
+                                  orderby m.Id descending
+                                  select m;
+                return PartialView(productlist);
+            }
+        }
 
-        //public ActionResult AddQuotedPartial(int clientId)
-        //{
-        //    var quoted = _db.SP_Quoted.SingleOrDefault(m => m.SP_Client.Id == clientId);
-        //    return PartialView(quoted);
-        //}
+        public ActionResult AddQuotedPartial(int clientId)
+        {
+            var quoted = _db.SP_Quoted.SingleOrDefault(m => m.SP_Client.Id == clientId);
+            return PartialView(quoted);
+        }
     }
 }
