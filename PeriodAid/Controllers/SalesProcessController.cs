1f100753f433844eff7926ded9509c967ea9f12f
524671516 <524671516@qq.com>
2017-12-27 16:55:58 +0800

    121

--- PeriodAid/Controllers/SalesProcessController.cs
+++ PeriodAid/Controllers/SalesProcessController.cs
@@ -1244,7 +1244,7 @@ namespace PeriodAid.Controllers
         public JsonResult AllProductAjax(int clientId)
         {
             var product = from m in _db.SP_QuotePrice
-                          where m.Quoted_Status != -1
+                          where m.Quoted_Status != -1 && m.SP_SalesSystem.Client_Id == clientId
                           select new { Id = m.Id, ItemName = m.SP_Product.Item_Name };
             return Json(new { result = "SUCCESS", data = product }, JsonRequestBehavior.AllowGet);
         }
