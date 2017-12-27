648956965eea6823cfd52944ca003b9fefe6e26b
myron <1207278908@qq.com>
2017-12-27 16:52:41 +0800

    9

--- PeriodAid/Controllers/SalesProcessController.cs
+++ PeriodAid/Controllers/SalesProcessController.cs
@@ -12,6 +12,8 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace PeriodAid.Controllers
@@ -1136,7 +1138,7 @@ namespace PeriodAid.Controllers
 
         }
         
-        public ActionResult OrderPriceList(int orderId,int clientId)
+        public ActionResult OrderPriceList(int orderId)
         {
             var order = (from m in _db.SP_Order
                          where m.Id == orderId
@@ -1242,7 +1244,7 @@ namespace PeriodAid.Controllers
         public JsonResult AllProductAjax(int clientId)
         {
             var product = from m in _db.SP_QuotePrice
-                          where m.Quoted_Status != -1 && m.SP_SalesSystem.Client_Id == clientId
+                          where m.Quoted_Status != -1
                           select new { Id = m.Id, ItemName = m.SP_Product.Item_Name };
             return Json(new { result = "SUCCESS", data = product }, JsonRequestBehavior.AllowGet);
         }
