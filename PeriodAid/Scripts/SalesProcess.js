beeba72b311009af9c4c70a2370271355d499ab4
524671516 <524671516@qq.com>
2018-01-19 17:59:07 +0800

    121

--- PeriodAid/Scripts/SalesProcess.js
+++ PeriodAid/Scripts/SalesProcess.js
@@ -387,7 +387,19 @@
             throw new Error("onpagechanged is not a function");
         }
     }
-    // 多个model关闭
+    // 删除数组中的某个元素
+    Array.prototype.indexOf = function(val) {
+            for (var i = 0; i < this.length; i++) {
+            if (this[i] == val) return i;
+            }
+            return -1;
+            };
+    Array.prototype.remove = function(val) {
+            var index = this.indexOf(val);
+            if (index > -1) {
+            this.splice(index, 1);
+            }
+            };  
     
 
     TmDatePicker.defaultOptions = {
