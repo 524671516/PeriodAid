44d436a913fc4d9e9ea1edd60c95ae372ca5d0af
524671516 <524671516@qq.com>
2018-02-12 09:47:11 +0800

    33

--- PeriodAid/Models/IKCRMDATAModel.cs
+++ PeriodAid/Models/IKCRMDATAModel.cs
@@ -45,9 +45,9 @@ namespace PeriodAid.Models
 
         public string customer_name { get; set; }
 
-        public DateTime created_at { get; set; }
+        public DateTime? created_at { get; set; }
 
-        public DateTime updated_at { get; set; }
+        public DateTime? updated_at { get; set; }
     }
     [Table("CRM_ContractDetail")]
     public partial class CRM_ContractDetail
