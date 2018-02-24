b790703e65c45c750a77b6839070c0e87ac1247c
myron <1207278908@qq.com>
2018-02-24 14:31:55 +0800

    222

--- PeriodAid/Models/IKCRMDATAModel.cs
+++ PeriodAid/Models/IKCRMDATAModel.cs
@@ -20,6 +20,7 @@ namespace PeriodAid.Models
         public virtual DbSet<CRM_Product> CRM_Product { get; set; }
         public virtual DbSet<CRM_Contract> CRM_Contract { get; set; }
         public virtual DbSet<CRM_ContractDetail> CRM_ContractDetail { get; set; }
+        public virtual DbSet<CRM_User_Token> CRM_User_Token { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             
@@ -83,7 +84,15 @@ namespace PeriodAid.Models
 
         public int product_quantity { get; set; }
     }
+    [Table("CRM_User_Token")]
+    public partial class CRM_User_Token
+    {
+        public int Id { get; set; }
 
+        public string user_token { get; set; }
+
+        public DateTime download_at { get; set; }
+    }
     public class orders_Result
     {
         public bool success { get; set; }//响应成功/响应失败
