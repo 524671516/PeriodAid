38cab69a7b2de73436fcdcd142691243a27985b7
524671516 <524671516@qq.com>
2018-03-13 11:59:38 +0800

    2.1

--- PeriodAid/Models/IKCRMDATAModel.cs
+++ PeriodAid/Models/IKCRMDATAModel.cs
@@ -32,132 +32,27 @@ namespace PeriodAid.Models
 
         }
     }
-    //[Table("CRM_Product")]
-    //public partial class CRM_Product
-    //{
-    //    public int Id { get; set; }
-        
-    //    [StringLength(64)]
-    //    public string system_Code { get; set; }
 
-    //    [StringLength(64)]
-    //    public string item_Name { get; set; }
-        
-    //    public int product_id { get; set; }
-    //    // 规格
-    //    public string product_standard { get; set; }
-    //    // 如箱数
-    //    public string enter_box { get; set; }
-    //    // 单价
-    //    public decimal unit_price { get; set; }
-    //    // 毛利
-    //    public string gross_profit { get; set; }
-    //    // 零售价
-    //    public decimal retail_price { get; set; }
-    //    // 单盒条形码
-    //    public string product_barcode { get; set; }
-    //    // QS/SC号
-    //    public string QS_SC_number { get; set; }
-    //    // 保质期
-    //    public string expiration_date { get; set; }
-    //    // 单包装尺寸
-    //    public string product_size { get; set; }
-    //    // 外箱条码
-    //    public string outside_barcode { get; set; }
-    //    // 外箱包装尺寸
-    //    public string outside_size { get; set; }
-    //    // 净重
-    //    public decimal net_weight { get; set; }
-    //    // 毛重
-    //    public decimal gross_weight { get; set; }
-    //}
-
-    //public partial class CRM_Product_ReturnData
-    //{
-    //    public string code { get; set; }
-
-    //    public CRM_ProductData data { get; set; }
-    //}
-
-    //public partial class CRM_ProductData
-    //{
-    //    public int per_page { get; set; }
-
-    //    public int total_count { get; set; }
-
-    //    public List<CRM_ProductCategories> product_categories { get; set; }
-
-    //    public List<CRM_ProductList> products { get; set; }
-    //}
-
-    //public partial class CRM_ProductCategories
-    //{
-    //    public string name { get; set; }
-    //}
-
-    //public partial class CRM_ProductList
-    //{
-    //    public int id { get; set; }
-
-    //    public string product_no { get; set; }
-
-    //    public string name { get; set; }
-    //    // 规格
-    //    public string text_asset_ccc3d6 { get; set; }
-    //    // 如箱数
-    //    public string text_asset_ed9843 { get; set; }
-    //    // 单价
-    //    public decimal unit_cost { get; set; }
-    //    // 毛利
-    //    public string gross_margin { get; set; }
-    //    // 零售价
-    //    public decimal standard_unit_price { get; set; }
-    //    // 单盒条形码
-    //    public string text_asset_a632bd { get; set; }
-    //    // QS/SC号
-    //    public string text_asset_3a7a67 { get; set; }
-    //    // 保质期
-    //    public string text_asset_1c439d { get; set; }
-    //    // 单包装尺寸
-    //    public string text_asset_2e3eb8 { get; set; }
-    //    // 外箱条形码
-    //    public string text_asset_d00266 { get; set; }
-    //    // 外箱包装尺寸
-    //    public string text_asset_ce85c8 { get; set; }
-    //    // 净重
-    //    public string numeric_asset_db2cc5 { get; set; }
-    //    // 毛重
-    //    public string numeric_asset_4958a4 { get; set; }
-    //    // 分类
-    //    public CRM_ProductCategory product_category { get; set; }
-        
-    //}
-
-    //public partial class CRM_ProductCategory
-    //{
-    //    public string name { get; set; }
-    //}
-    
     [Table("CRM_User_Token")]
     public partial class CRM_User_Token
     {
         public int Id { get; set; }
-        
+
         public string Key { get; set; }
         [StringLength(256)]
         public string Value { get; set; }
 
         public DateTime download_at { get; set; }
-        
+
     }
-    
+
     public partial class CRM_Contract_ReturnData
     {
         public string code { get; set; }
 
         public CRM_Contract_Data data { get; set; }
     }
-    
+
     public partial class CRM_Contract_Data
     {
         public List<CRM_ContractResult> contracts { get; set; }
@@ -188,17 +83,17 @@ namespace PeriodAid.Models
     public class CRM_Contract
     {
         public int id { get; set; }
-        
+
         public int contract_id { get; set; }
 
         [StringLength(64)]
         public string platform_code { get; set; }
-        
+
         public int user_id { get; set; }
 
         [StringLength(64)]
         public string user_name { get; set; }
-        
+
         public int customer_id { get; set; }
 
         public virtual CRM_Customer CRM_Customer { get; set; }
@@ -207,25 +102,24 @@ namespace PeriodAid.Models
         public string contract_title { get; set; }
 
         public decimal total_amount { get; set; }
-
+        [StringLength(32)]
         public string contract_status { get; set; }
+        [StringLength(256)]
+        public string contract_remark { get; set; }
 
         public DateTime? updated_at { get; set; }
-
         [StringLength(256)]
         public string express_information { get; set; }
-
         [StringLength(64)]
         public string express_code { get; set; }
         [StringLength(32)]
         public string express_status { get; set; }
-        
+        [StringLength(256)]
+        public string express_remark { get; set; }
         [StringLength(128)]
         public string shop_code { get; set; }
-
         [StringLength(128)]
         public string warehouse_code { get; set; }
-
         [StringLength(128)]
         public string vip_code { get; set; }
 
@@ -257,13 +151,13 @@ namespace PeriodAid.Models
 
 
     }
-    
+
     [Table("CRM_ExceptionLogs")]
     public partial class CRM_ExceptionLogs
     {
         public int Id { get; set; }
 
-        public string type{ get; set; }
+        public string type { get; set; }
 
         public string exception { get; set; }
 
@@ -280,7 +174,7 @@ namespace PeriodAid.Models
     public partial class CRM_ContractDetail_Data
     {
         public List<CRM_ContractDetail_CustomerProductList> product_assets_for_new_record { get; set; }
-        
+
         public int id { get; set; }
 
         public string text_asset_1e30f4 { get; set; }
@@ -310,12 +204,11 @@ namespace PeriodAid.Models
         public string name { get; set; }
     }
     
-
     [Table("CRM_ContractDetail")]
     public partial class CRM_ContractDetail
     {
         public int Id { get; set; }
-        
+
         public int contract_id { get; set; }
 
         public virtual CRM_Contract CRM_Contract { get; set; }
@@ -331,14 +224,14 @@ namespace PeriodAid.Models
         public decimal unit_price { get; set; }
         // 0存在 -1删除
         public int status { get; set; }
-        
+
     }
 
     [Table("CRM_Customer")]
     public partial class CRM_Customer
     {
         public int Id { get; set; }
-        
+
         public int customer_id { get; set; }
         [StringLength(64)]
         public string customer_name { get; set; }
@@ -398,7 +291,7 @@ namespace PeriodAid.Models
         public string region_info { get; set; }
 
         public string tel { get; set; }
-        
+
         public string wechat { get; set; }
     }
 
@@ -408,7 +301,7 @@ namespace PeriodAid.Models
 
         public string name { get; set; }
     }
-    
+
     public partial class CRM_CustomerContactsAddress
     {
         public int addressable_id { get; set; }
@@ -424,7 +317,7 @@ namespace PeriodAid.Models
     public partial class CRM_Contact
     {
         public int Id { get; set; }
-        
+
         public int contact_id { get; set; }
 
         [StringLength(64)]
@@ -531,5 +424,5 @@ namespace PeriodAid.Models
         public string mail_no { get; set; }
 
     }
-    
+
 }
